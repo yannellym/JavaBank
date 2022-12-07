6e328ac7a0ad53934dab0e884ac6e0e1492670a1
yannelly25 <yannellym@gmail.com>
2022-12-07 15:19:46 -0500

    bank utilities class added

---
+++ src/BankUtilities.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BankUtilities {
+    Scanner scanner = new Scanner(System.in);
+
+    /**
+     *
+     * @param prompt a String that represents a prompt to print on the screen. Ex "Enter your name"
+     * @return String. The input of the user.
+     */
+    public String promptUserForString(String prompt){
+        System.out.println(prompt);
+        String name = scanner.next();
+        return name;
+    }
+
+    /**
+     * @param prompt
+     * @return a DOUBLE representing a positive number
+     */
+    public double promptUserForPositiveNumber(String prompt){
+        System.out.println(prompt);
+        Double positiveNum = scanner.nextDouble();
+
+        while (positiveNum <= 0){
+            System.out.println("Amount cannot be negative. Try again. ");
+            return promptUserForPositiveNumber(prompt);
+        }
+        return positiveNum;
+    }
+    public long convertFromDollarsToCents(double dollars){
+        return (long) dollars;
+    }
+
+    /**
+     * @params minVal,  maxVal are both of type int. They represent the range
+     * @return an int representing a random number
+     */
+    public int generateRandomInteger( int minVal, int maxVal){
+        int randomNumber = ThreadLocalRandom.current().nextInt(minVal, maxVal + 1);
+        return randomNumber;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * In this project you will tie together many of the concepts that you have learned throughout the
+ *         course into a unified banking application.  You will develop a program that performs many
+ *         common bank transactions that a bank employee or customer might perform on one or more
+ *         accounts.
+ *         When executed, the program will display a menu with the following choices as follows:
+ *         ============================================================
+ *         What do you want to do?
+ *         1. Open an account
+ *         2. Get account information and balance
+ *         3. Change PIN
+ *         4. Deposit money in account
+ *         5. Transfer money between accounts
+ *         6. Withdraw money from account
+ *         7. ATM withdrawal
+ *         8. Deposit change
+ *         9. Close an account
+ *         10. Add monthly interest to all accounts
+ *         11. End Program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        String prompt = "Please enter a number: ";
+        BankUtilities utilities = new BankUtilities();
+        // Double positiveNum = utilities.promptUserForPositiveNumber(prompt);
+        // int randomNum = utilities.generateRandomInteger(1,5);
+        long wholeDollar = utilities.convertFromDollarsToCents(3.57);
+        System.out.println(wholeDollar);
+
+
+    }
+}
