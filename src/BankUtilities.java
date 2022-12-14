ba5c04188f4ec01c68792bc232b04279fffe6f30
yannelly25 <yannellym@gmail.com>
2022-12-14 18:24:11 -0500

    fixing the promptforaccnumberandpin func

--- src/BankUtilities.java
+++ src/BankUtilities.java
@@ -1,49 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class BankUtilities extends Bank{
+public class BankUtilities extends Bank {
     Scanner scanner = new Scanner(System.in);
-
-
-    /**
-     *
-     * @param prompt a String that represents a prompt to print on the screen. Ex "Enter your name"
-     * @return String. The input of the user.
-     */
-    public String promptUserForString(String prompt){
-        System.out.println(prompt);
-        String name = scanner.next();
-        return name;
+    public int generateRandomInteger(int minVal, int maxVal) {
+        int randomNumber = ThreadLocalRandom.current().nextInt(minVal, maxVal + 1);
+        return randomNumber;
     }
-
-    /**
-     * @param prompt
-     * @return a DOUBLE representing a positive number
-     */
-    public double promptUserForPositiveNumber(String prompt){
+    public double promptUserForPositiveNumber(String prompt) {
         System.out.println(prompt);
         Double positiveNum = scanner.nextDouble();
 
-        while (positiveNum <= 0){
+        while (positiveNum <= 0) {
             System.out.println("Amount cannot be negative. Try again. ");
             return promptUserForPositiveNumber(prompt);
         }
         return positiveNum;
     }
-    public long convertFromDollarsToCents(double dollars){
-
-        return (long) dollars;
+    public String promptUserForString(String prompt) {
+        System.out.println(prompt);
+        String name = scanner.next();
+        return name;
     }
-
-    /**
-     * @params minVal,  maxVal are both of type int. They represent the range
-     * @return an int representing a random number
-     */
-    public int generateRandomInteger( int minVal, int maxVal){
-        int randomNumber = ThreadLocalRandom.current().nextInt(minVal, maxVal + 1);
-        return randomNumber;
-    }
-    public void openAccount(){
+    public void openAccount() {
         System.out.println("opening account ...");
         String firstName = promptUserForString("Enter your first name: ");
         String lastName = promptUserForString("Enter your last name: ");
@@ -53,72 +33,90 @@ public class BankUtilities extends Bank{
         int num = 0;
         String randomNums = "";
 
-        while( num < 10){
-            randomNums = randomNums + generateRandomInteger(0,9);
+        while (num < 10) {
+            randomNums = randomNums + generateRandomInteger(0, 9);
             num = num + 1;
         }
         long acc_number = Long.parseLong(randomNums);
-        Account newAccount = new Account(acc_number,firstName,lastName, SSN, 999);
+        Account newAccount = new Account(acc_number, firstName, lastName, SSN, 999);
         // System.out.println(newAccount.getClass().getSimpleName());
         all_accounts.add(newAccount);
         System.out.println("Account " + newAccount.accNumber + " was created.");
     }
-    public void getAccountInfoAndBalance(){
+    public ArrayList<Object> promptForAccountNumberAndPIN() {
+        ArrayList<Object> res = new ArrayList<Object>();
+
         System.out.println("Please enter an account number: ");
         long accNumber = scanner.nextLong();
-        for (Account singleAccount : all_accounts){
-            if (singleAccount.accNumber == accNumber){
-                System.out.println("Please enter PIN:");
-                int accPin = scanner.nextInt();
-                if(accPin != singleAccount.PIN){
+
+        System.out.println("Please enter PIN:");
+        int accPin = scanner.nextInt();
+
+        // add both variables to the arrayList
+        res.add(accNumber);
+        res.add(accPin);
+
+        // returns an arrayList of two values (account number, and account pin)
+        return res;
+    }
+    public void getAccountInfoAndBalance() {
+        // call the promptForAccountNumberAndPin function which returns an ArrayList
+        // save the response above in an arr called info of type ArrayList<Object>
+        ArrayList<Object> info = promptForAccountNumberAndPIN();
+
+        // the two values accessed from the info arrayList are cast into the correct type
+        long accNumber = (long) info.get(0);
+        int accPin = (int) info.get(1);
+
+        for (Account singleAccount : all_accounts) {
+            if (singleAccount.accNumber == accNumber) {
+                if (accPin != singleAccount.PIN) {
                     System.out.println("Invalid PIN");
                     getAccountInfoAndBalance();
                 }
-                System.out.println(singleAccount.balance);
-                return;
+            } else {
+                System.out.println("Account not found for account number:" + accNumber);
+                getAccountInfoAndBalance();
             }
         }
-        System.out.println("Account not found for account number:" + accNumber);
-        getAccountInfoAndBalance();
+        return true;
     }
-    public void changePin(){
+    }
+    public long convertFromDollarsToCents(double dollars) {
+
+        return (long) dollars;
+    }
+    public void changePin() {
+
         System.out.println("Changing PIN");
     }
-    public void depositMoneyToAccount(){
+
+    public void depositMoneyToAccount() {
         System.out.println("Depositing Money to account");
     }
-    public void transferBetweenAccounts(){
+
+    public void transferBetweenAccounts() {
         System.out.println("Transferring money between accounts");
     }
-    public void withdrawFromAccount(){
+
+    public void withdrawFromAccount() {
         System.out.println("Withdrawing money from account");
     }
-    public void withdrawFromATM(){
+
+    public void withdrawFromATM() {
         System.out.println("Withdrawing money from ATM");
     }
-    public void depositChange(){
+
+    public void depositChange() {
         System.out.println("Depositing Change to account");
     }
-    public void closeAccount(){
+
+    public void closeAccount() {
         System.out.println("Closing account");
     }
-    public void addMonthlyInterest(){
+
+    public void addMonthlyInterest() {
         System.out.println("Adding a monthly interest to all accounts");
     }
-    public Account promptForAccountNumberAndPIN(){
-        System.out.println("Please enter an account number: ");
-        long accNumber = scanner.nextLong();
-        for (Account singleAccount : all_accounts){
-            if (singleAccount.accNumber == accNumber){
-                System.out.println("Please enter PIN:");
-                int accPin = scanner.nextInt();
-                if(accPin != singleAccount.PIN){
-                    System.out.println("Invalid PIN");
-                }
-                openAccount();
-            }
-        }
-        System.out.println("Account not found for account number:" + accNumber);
-        return null;
-    }
+
 }
